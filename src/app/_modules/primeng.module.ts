b9f981654ccccced01ad5e0a489f8828e82b2f4d
r3rivera <r.ryan.rivera@outlook.com>
2020-08-14 19:17:14 -0500

    Added landing components

--- src/app/_modules/primeng.module.ts
+++ src/app/_modules/primeng.module.ts
@@ -14,6 +14,7 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
 
 
 const primeModules = [
@@ -31,7 +32,8 @@ const primeModules = [
   DropdownModule,
   RadioButtonModule,
   BreadcrumbModule,
-  MenubarModule
+  MenubarModule,
+  CardModule
 ];
 
 @NgModule({
