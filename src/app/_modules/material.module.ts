f8e171a4188938378919afaf30c2fe2833d55310
r3rivera <r.ryan.rivera@outlook.com>
2020-08-16 13:35:46 -0500

    Added login form

--- src/app/_modules/material.module.ts
+++ src/app/_modules/material.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from "@angular/core";
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 const materialModules = [
   MatIconModule,
   MatToolbarModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatInputModule,
   MatMenuModule,
   MatSliderModule,
   MatButtonModule,
